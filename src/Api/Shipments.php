813de7453aeb70bd3918ab1497701e210b7dd0e8
SchoppAx <develop4all@gmail.com>
2019-07-07 15:01:21 +0200

    Update Shipments.php
    Remove post path

--- src/Api/Shipments.php
+++ src/Api/Shipments.php
@@ -7,25 +7,25 @@ class Shipments extends Api
     public function create($body, $parameters = [])
     {
         $body = [ 'function' => 'create' ] + $body;
-        return $this->post('/', $parameters, $body);
+        return $this->post('', $parameters, $body);
     }
 
     public function modify($snr, $body)
     {
         $body = [ 'function' => 'modify', 'snr' => $snr ] + $body;
-        return $this->post('/', [], $body);
+        return $this->post('', [], $body);
     }
 
     public function remove($snr)
     {
         $body = [ 'function' => 'delete', 'snr' => $snr ];
-        return $this->post('/', [], $body);
+        return $this->post('', [], $body);
     }
 
     public function export($snr, $load)
     {
         $body = [ 'function' => 'export', 'snr' => $snr, 'load' => $load ];
-        return $this->post('/', [], $body);
+        return $this->post('', [], $body);
     }
 
     public function setstatus($snr, $load)
