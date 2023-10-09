e8cf28a599b1fb7977bda7df7bb836d8ee0967e5
Axel <develop4all@gmail.com>
2023-10-09 18:34:02 +0200

    :memo: Add error handling

--- example.php
+++ example.php
@@ -10,24 +10,28 @@ $gel = new mehrWEBnet\Gel\Gel('api-key', 'depotNr', 'knr', ['test']);
 // List all carriers
 // var_dump($gel->shipmentQuotes()->create('[parameters]'));
 
-var_dump($gel->shipments()->create([
-    'sname1'    => 'company name',
-    'sname1'    => 'first name',
-    'sname2'    => 'lastname',
-    'sstreet'   => 'street',
-    'scountry'  => 'D',
-    'szipcode'  => 'zip_code',
-    'stown'     => 'city',
-    'cname1'    => 'name',
-    'cstreet'   => 'street',
-    'ccountry'  => 'D',
-    'ctown'     => 'city',
-    'srv'       => '2',
-    'xsrv>'     => 'A'
-    'collicnt'  => 'int',
-    'colli'     => '|weight|length|width|height', // in cm
-    'avis'      => 'phone',
-    'atype'    => 'A3',
-    'email'    => 'mail',
-    'rcall'    => ''
-]));
+try {
+    var_dump($gel->shipments()->create([
+        'sname1'    => 'company name',
+        'sname1'    => 'first name',
+        'sname2'    => 'lastname',
+        'sstreet'   => 'street',
+        'scountry'  => 'D',
+        'szipcode'  => 'zip_code',
+        'stown'     => 'city',
+        'cname1'    => 'name',
+        'cstreet'   => 'street',
+        'ccountry'  => 'D',
+        'ctown'     => 'city',
+        'srv'       => '2',
+        'xsrv>'     => 'A',
+        'collicnt'  => 'int',
+        'colli'     => '|weight|length|width|height', // in cm
+        'avis'      => 'phone',
+        'atype'    => 'A3',
+        'email'    => 'mail',
+        'rcall'    => ''
+    ]));    
+} catch(GuzzleHttp\Exception\ClientException $t) {
+    var_dump($t);
+}
